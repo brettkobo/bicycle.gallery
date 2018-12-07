1f1aa592310a53f2dc009c38110a5ec94ca3b5c4
brettkobo <brettkob@gmail.com>
2018-12-07 04:56:25 +0000

    Added more function to loop through data.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 Hi there! Welcome to AWS Cloud9!
 
 To get started, create some files, play with the terminal,
-or visit https://docs.aws.amazon.com/console/cloud9/ for our documentation.
+or visit sudo yum -y update   for our documentation.
 
 Happy coding!
 
---
+++ js/app.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    items: [
+            {
+              title: '2015 Bombtrack Hook 2',
+              image: 'https://files.bikeindex.org/uploads/Pu/42052/Bombtrack_Hook_2a.jpg',
+              description: 'My red demon.',
+              tags: ["cyclrocross", "dirt cruncher", "red", "sram"]
+            },
+            {
+              title: 'Junker 7',
+              image: 'https://i2.wp.com/tomsbiketrip.com/wp-content/uploads/2017/01/ridgeback-tour-2018.jpg?resize=750%2C444',
+              description: 'Tour Jam',
+              tags: ["touring bike", "slow moving"]
+            },
+            {
+              title: '2015 Bombtrack Hook 2',
+              image: 'https://i.pinimg.com/originals/b0/a4/f5/b0a4f5c77ab4dd48b66ab95ccdf581f2.jpg',
+              description: 'That phat bike I always wanted',
+              tags: ["mtb bike", "black", "cool"]
+            }
+    ]  
+  }
+})
+
